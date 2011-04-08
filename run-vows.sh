622cf3e01560a653d755bcacad263f8dea3d3c5f
Sami Hangaslammi <sami.hangaslammi@leonidasoy.fi>
2011-04-08 15:39:09 +0300

    run tests in NODE_ENV=test

--- run-vows.sh
+++ run-vows.sh
@@ -2,4 +2,4 @@
 mkdir -p vows-js
 rm -f vows-js/*
 ./node_modules/coffee-script/bin/coffee --compile --output vows-js vows
-./node_modules/vows/bin/vows vows-js/* $1
+NODE_ENV=test ./node_modules/vows/bin/vows vows-js/* $1
