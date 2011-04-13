1c32bda5aef2983ce4829901b986440bd90cd24c
Tomi Laine <tomi.laine@leonidasoy.fi>
2011-04-13 14:46:33 +0300

    basic deployment scripts with forever-module as supervisor

--- config/deploy.rb
+++ config/deploy.rb
@@ -12,6 +12,8 @@ set :repository, "http://git.gitorious.org/meego-quality-assurance/qa-dashboard.
 set :deploy_via, :remote_cache
 
 set :public_children, %w(img css js)
+set :start_script, "./run-server.sh"
+
 
 ssh_options[:forward_agent] = true
 ssh_options[:user] = "www-data"
@@ -21,17 +23,17 @@ after "deploy:finalize_update", "deploy:install_node_packages"
 namespace :deploy do
   desc "Restart the app server"
   task :restart, :roles => :app do
-    #run "cd #{current_path} && run-server.sh --forever stop && run-server.sh --forever start"
+    run "cd #{current_path} && #{start_script} --forever stop && #{start_script} --forever start"
   end
 
   desc "Start the app server"
   task :start, :roles => :app do
-    #run "cd #{current_path} && run-server.sh --forever stop && run-server.sh --forever start"
+    run "cd #{current_path} && #{start_script} --forever start"
   end
 
   desc "Stop the app server"
   task :stop, :roles => :app do
-    #run "cd #{current_path} && run-server.sh --forever stop"
+    run "cd #{current_path} && #{start_script} --forever stop"
   end
 
   desc "Install node packages"
