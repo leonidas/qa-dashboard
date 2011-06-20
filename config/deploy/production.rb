503ea8f8164dcbaa83dd7bfea61e2f98a9310791
Sami Hangaslammi <sami.hangaslammi@leonidasoy.fi>
2011-06-20 15:46:15 +0300

    updated production config

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,12 +1,12 @@
 set :app_name, "qa-dashboard"
-set :server_host, "#{app_name}.leonidasoy.fi"
-set :server_port, 3030
+set :server_host, "#{app_name}"
+set :server_port, 8000
 
 set :application, server_host
 set :deploy_to, "/home/#{user}/#{application}"
 set :node_env, "production"
 
-ssh_options[:port] = 43398
+ssh_options[:port] = 23
 
 server server_host, :app, :web, :db, :primary => true
 
