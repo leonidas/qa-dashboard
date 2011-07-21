0414a8e4d954c24460658f0ab5d60c252eebfc06
Tomi Laine <tomi.laine@leonidasoy.fi>
2011-07-21 17:03:48 +0300

    compile all coffeescript in step definitions folder

--- run-cucumis.sh
+++ run-cucumis.sh
@@ -1,3 +1,3 @@
 #!/bin/sh -x
-./node_modules/coffee-script/bin/coffee -c ./features/step_definitions/login.coffee
+./node_modules/coffee-script/bin/coffee -c ./features/step_definitions
 NODE_ENV=test node_modules/cucumis/bin/cucumis -t 15000
