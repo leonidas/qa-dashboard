3e8ec883a7265c6a33a1c109077555cbb2c4e29f
Vesa Poikajärvi <vesa.poikajarvi@leonidasoy.fi>
2013-04-11 10:22:57 +0300

    Run tests with npm, don't compile to JS
    nodeunit supports CoffeeScript so no need to compile. Also separate
    script is pretty much useless when we have npm.

--- run-nodeunit.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-mkdir -p test-js
-rm -f test-js/*
-./node_modules/coffee-script/bin/coffee --compile --output test-js test
-NODE_PATH=server NODE_ENV=test ./node_modules/nodeunit/bin/nodeunit test-js/* $1 $2 $3 $4 $5
