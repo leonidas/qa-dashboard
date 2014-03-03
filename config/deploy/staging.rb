1ef1702b0f93b08a4afff04a28a52e81f862d857
Vesa Poikajärvi <vesa.poikajarvi@leonidasoy.fi>
2014-03-03 13:26:03 +0700

    Remove db:dump from staging config

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -8,17 +8,3 @@ set :node_env,     'production'
 set :keep_releases, 10
 
 server 'localhost', user: fetch(:user), roles: %w{web app db}
-
-namespace :db do
-  desc 'Dump and fetch production database'
-  task :dump do
-    on roles(:db) do
-      within current_path do
-        execute :mongodump, '--db', 'qadash-production'
-        execute :tar, '-czf', 'qadash-production.tar.gz', './dump/qadash-production'
-        download! "#{current_path}/qadash-production.tar.gz", "./qadash-production.tar.gz"
-        execute :rm, 'qadash-production.tar.gz'
-      end
-    end
-  end
-end
